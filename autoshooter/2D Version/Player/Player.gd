45b982880ff9ac36576162e4d0f4b4a40eaad0bc
Talamander <toxcrewproductions@gmail.com>
2022-11-11 20:33:39 -0600

    puuush

--- autoshooter/2D Version/Player/Player.gd
+++ autoshooter/2D Version/Player/Player.gd
@@ -2,13 +2,17 @@ extends KinematicBody2D
 
 export var rotation_speed:= 90.0
 
+onready var animPlayer = $AnimationPlayer
+
 var motion = Vector2.ZERO
 var acceleration:= 5000
 var speed:= 360.0
 var target = Vector2.ZERO
 var active = false
 
-
+func _ready():
+	pass
+	
 func _physics_process(delta: float) -> void:
 	var input_vector = get_input_vector()
 	
@@ -16,17 +20,23 @@ func _physics_process(delta: float) -> void:
 	#Vector2.ZERO is true when no move key is being pressed
 	if input_vector == Vector2.ZERO:
 		apply_friction(acceleration * delta)
+		animPlayer.play("Idlke")
 	else:
 		calc_movement(input_vector * acceleration * delta)
+		animPlayer.play("Run")
 		
 	motion = move_and_slide(motion)
 	
+	draw_shadow()
 	
+	
+	#checking if there is a body within the area2d -> aka 'active targeting'
 	if !active:
 		return
 	else:
 		$Position2D.look_at(target)
 	
+	animHandler()
 	#rotate_ray(delta)
 
 
@@ -40,7 +50,17 @@ func get_input_vector():
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	return input_vector.normalized()
-	
+
+func animHandler():
+	if motion.x == 0:
+		animPlayer.play("Idlke")
+	elif motion.x > 0:
+		$Sprite.set_flip_h(false)
+	elif motion.x < 0:
+		$Sprite.set_flip_h(true)
+	else:
+		pass
+
 func apply_friction(amount):
 	#Get the player movement moving smoothly
 	if motion.length() > amount:
@@ -61,3 +81,12 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_body_exited(body):
 	active = false
 	pass # Replace with function body.
+
+
+#this function is just to practice drawing shapes with gdScript
+func draw_shadow() -> void:
+	var center = Vector2(100,100)
+	var rad = 100
+	var col = Color(255,0,255)
+	draw_circle(center, rad, col)
+	print("shadow should be here")
