95806ce25fe8221e636ea03cc547704c762ffd58
Talamander <toxcrewproductions@gmail.com>
2022-11-12 06:50:39 -0600

    Documentation

--- autoshooter/2D Version/Parent Classes/RayController.gd
+++ autoshooter/2D Version/Parent Classes/RayController.gd
@@ -10,12 +10,14 @@ var active = false
 
 
 func _physics_process(delta):
+	
+	#DEBUG
 	if Input.is_action_just_pressed("ui_accept"):
 		print("Rays: ", ray_list)
 		var delete = ray_list.pop_back()
 		delete.enabled = false
 	
-	#check for collision
+	
 	
 	update_raycasts()
 	
@@ -26,17 +28,16 @@ func _physics_process(delta):
 			return
 
 
-
+#Generating the raycasts via code
 func generate_raycasts(amount: int) -> void:
 	
 	for i in amount:
 		var ray = RayCast2D.new()
 		
-		#Just doing this for fun
 		var angle = deg2rad(90)
 		ray.cast_to = Vector2.UP.rotated(angle) * max_ray_distance
 		add_child(ray)
-		ray.enabled = true
+		ray.enabled = true #I think we can somehow use if it's enabled/disabled to track which ones are in use
 		ray_list.append(ray)
 		#ray.set_collision_mask_bit(2)
 	
@@ -48,8 +49,8 @@ func update_raycasts():
 
 func aim_raycasts():
 	#aim raycast at closest free enemy
-	var target = get_closest_enemy()
-	var raycast = get_raycast()
+	var target = get_closest_enemy() #that isn't targeted
+	var raycast = get_raycast() #that isn't actively targeting
 	
 	raycast.target = target
 	
@@ -76,7 +77,7 @@ func get_closest_enemy() -> KinematicBody2D:
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("enemy"):
 		target_list.append(body)
-		active = true
+		active = true #just a check
 
 
 func _on_Area2D_body_exited(body):
